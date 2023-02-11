3ec53df9ac4f9f1cef62f2b04c1c5e1315c8569a
Desarrollador Gemp <desarrollador.gemp@gmail.com>
2023-02-10 19:01:53 -0600

    Init: Frontend dashboard React

---
+++ src/axios-client.js
@@ -0,0 +1,27 @@
+const { default: axios } = require("axios");
+
+const axiosClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+})
+
+axiosClient.interceptors.require.use((config) => {
+  const token = localStorage.get('ACCESS_TOKEN');
+  config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+})
+
+axiosClient.interceptors.response.use((response) => {
+
+  return response;
+
+}, (error) => {
+  const {response} = error;
+  if (response.status === 401) {
+    localStorage.removeItem('ACCESS_TOKEN');
+  }
+
+  throw error;
+})
+
+export default axiosClient;
---
+++ src/components/DefaultLayout.jsx
@@ -0,0 +1,39 @@
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+import Login from '../views/Login'
+
+export default function DefaultLayout() {
+  const {user, token} = useStateContext();
+
+  if (!token) {
+    return <Navigate to="/login" />
+  }
+
+  const onLogout = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+
+      <div className="content">
+        <header>
+          <div>
+            Header
+          </div>
+          <div>
+            { user.name }
+            <a className="btn-logout" onClick={onLogout} href="#">Logout</a>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
